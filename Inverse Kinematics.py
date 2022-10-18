342b862c0b388d6db869af09de95c029490a674e
caseydw2 <caseywheatonwerle@gmail.com>
2022-10-17 20:10:54 -0500

    Change string into class to add multi strings

--- Inverse Kinematics.py
+++ Inverse Kinematics.py
@@ -7,6 +7,7 @@ Created on Mon Mar 14 16:45:35 2022
 import numpy as np
 import pygame as pg
 
+# Initialize Colors
 RED = (225, 0, 0)
 ORANGE = (255, 127, 0)
 YELLOW = (225, 225, 0)
@@ -18,6 +19,12 @@ WHITE = (225, 225, 225)
 BLACK = (0, 0, 0)
 
 Colors = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET]
+
+# Initialize window size
+WIDTH = 700
+HEIGHT = 700
+size = (WIDTH, HEIGHT)
+T_num = 3
 
 
 class Segment:
@@ -46,7 +53,7 @@ class Segment:
         self.apos = self.get_a()
 
 
-def list_seg(num):
+def list_seg(num, factor):
     # Create a list of segments where the (i+1)th segments b position is the
     # ith a position.
     seglist = [Segment(np.array([350, 350-(600/num)]),
@@ -54,22 +61,64 @@ def list_seg(num):
     for i in range(num-1):
         seglist.append(
             Segment(np.array([350, 350+(600/((i+1)*num))]), seglist[i].apos,
-                    (600/num), 0))
+                    (300/num), 0))
     return seglist
 
 
-# Initialize window size
-WIDTH = 700
-HEIGHT = 700
-size = (WIDTH, HEIGHT)
+class Tentacle:
+    def __init__(self, factor, seg_num, base):
+        self.factor = factor
+        self.seg_num = seg_num
+        self.base = base
+        self.segments = list_seg(seg_num, self.factor)
+
+    def update(self):
+        self.segments[0].bpos = np.array(pg.mouse.get_pos())
+        self.segments[0].apos = self.segments[0].get_a()
+
+        # Set every segments b position to the a position of the segment infront
+        # it.
+        for index, segment in enumerate(self.segments[1:]):
+            segment.update(self.segments[index].apos)
+
+        # Make the bottom segment fixed to the middle bottom of the screen
+        segmentsbkwrd = self.segments[::-1]
+        segmentsbkwrd[0].bpos = self.base
+        segmentsbkwrd[0].apos = segmentsbkwrd[0].get_a()
+
+        # Adjust the segments going from the bottom segment.
+        for index, segment in enumerate(segmentsbkwrd[1:]):
+            segment.bpos = segmentsbkwrd[index].apos
+            segment.apos = segment.get_a()
+
+    def show(self, screen):
+        i = 0
+        num = len(self.segments)
+        mod = int(num / 5)
+
+        for index, segment in enumerate(self.segments):
+            # Cycle through the colors with a "continuous" line between
+            # consecutive colors
+            if index % mod == 0:
+                i += 1
+                modi = index % mod
+                COLOR = ((1-modi/mod) * np.array(Colors[i-1]) + modi/mod *
+                         np.array(Colors[i]))
+                COLOR = tuple(COLOR)
+                segment.draw_segment(screen, COLOR, 2*i+4)
+
 
 # Create screen for screen
 screen = pg.display.set_mode(size)
 base = np.array((WIDTH/2, HEIGHT - 1))
 done = False
 clock = pg.time.Clock()
-
-seglist = list_seg(1000)
+tentacles = []
+for i in range(T_num):
+    theta = (2*np.pi / 3)*i
+    pos = np.array((100 * np.cos(theta) + WIDTH /
+                   2, 100*np.sin(theta) + HEIGHT/2))
+    tentacles.append(Tentacle(1, 200, pos))
 
 # While loop for quiting screen
 while not done:
@@ -78,44 +127,47 @@ while not done:
             done = True
     screen.fill(WHITE)
 
-    i = 0
-    num = len(seglist)
-    mod = int(num / 5)
+    for tentacle in tentacles:
+        tentacle.show(screen)
+    # i = 0
+    # num = len(seglist)
+    # mod = int(num / 5)
 
-    for index, segment in enumerate(seglist):
-        # Cycle through the colors with a "continuous" line between consecutive
-        # colors
-        WIDTH = (1 - index/num) * 2 + index/num * 5
-        if index % mod == 0:
-            i += 1
-        modi = index % mod
-        COLOR = ((1-modi/mod) * np.array(Colors[i-1]) + modi/mod *
-                 np.array(Colors[i]))
-        COLOR = tuple(COLOR)
-        segment.draw_segment(screen, COLOR, 2*i+4)
+    # for index, segment in enumerate(seglist):
+    #     # Cycle through the colors with a "continuous" line between consecutive
+    #     # colors
+    #     if index % mod == 0:
+    #         i += 1
+    #     modi = index % mod
+    #     COLOR = ((1-modi/mod) * np.array(Colors[i-1]) + modi/mod *
+    #              np.array(Colors[i]))
+    #     COLOR = tuple(COLOR)
+    #     segment.draw_segment(screen, COLOR, 2*i+4)
 
     # Update the screen
     pg.display.flip()
 
     # Have the first segment follow the mouse.
-    seglist[0].bpos = np.array(pg.mouse.get_pos())
-    seglist[0].apos = seglist[0].get_a()
+    # seglist[0].bpos = np.array(pg.mouse.get_pos())
+    # seglist[0].apos = seglist[0].get_a()
 
-    # Set every segments b position to the a position of the segment infront
-    # it.
-    for index, segment in enumerate(seglist[1:]):
-        segment.update(seglist[index].apos)
+    # # Set every segments b position to the a position of the segment infront
+    # # it.
+    # for index, segment in enumerate(seglist[1:]):
+    #     segment.update(seglist[index].apos)
 
-    # Make the bottom segment fixed to the middle bottom of the screen
-    seglistbkwrd = seglist[::-1]
-    seglistbkwrd[0].bpos = base
-    seglistbkwrd[0].apos = seglistbkwrd[0].get_a()
+    # # Make the bottom segment fixed to the middle bottom of the screen
+    # seglistbkwrd = seglist[::-1]
+    # seglistbkwrd[0].bpos = base
+    # seglistbkwrd[0].apos = seglistbkwrd[0].get_a()
 
-    # Adjust the segments going from the bottom segment.
-    for index, segment in enumerate(seglistbkwrd[1:]):
-        segment.bpos = seglistbkwrd[index].apos
-        segment.apos = segment.get_a()
+    # # Adjust the segments going from the bottom segment.
+    # for index, segment in enumerate(seglistbkwrd[1:]):
+    #     segment.bpos = seglistbkwrd[index].apos
+    #     segment.apos = segment.get_a()
 
+    for tentacle in tentacles:
+        tentacle.update()
     clock.tick(60)
 
 pg.quit()
