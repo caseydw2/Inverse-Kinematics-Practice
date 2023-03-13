966a8311cc30def1583c3b507d50eea4dbf29941
caseydw2 <caseywheatonwerle@gmail.com>
2023-03-13 15:56:14 -0500

    Add Bouncing ball animation for tentacles to follow.

--- Inverse Kinematics.py
+++ Inverse Kinematics.py
@@ -7,14 +7,19 @@ Created on Mon Mar 14 16:45:35 2022
 import numpy as np
 import pygame as pg
 from math import pi
-
+from BouncingBall import Ball
+#Tentacle Parameters
 Tentacle_Number = 10
-
 Radius = 300
+Segment_Number = 20
+Length = 200
 
-Segment_Number = 10
+#Window Parameters
+WIDTH = 1600
+HEIGHT = 1000
 
-Length = 250
+#Ball Parameters
+Follow_Ball = True
 
 RED = (225, 0, 0)
 ORANGE = (255, 127, 0)
@@ -30,16 +35,6 @@ GREY_DARK= (113,113,113)
 
 Colors = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET]
 
-# Initialize window size
-WIDTH = 1600
-HEIGHT = 1000
-size = (WIDTH, HEIGHT)
-CENTER = np.array([WIDTH/2,HEIGHT/2])
-# Create screen for screen
-screen = pg.display.set_mode(size)
-base = np.array((WIDTH/2, HEIGHT-1))
-done = False
-clock = pg.time.Clock()
 
 
 class Segment:
@@ -82,15 +77,15 @@ def list_seg(num,length = 600):
 
 
 class Tentacle:
-    def __init__(self,base,lenght) -> None:
+    def __init__(self,base,length) -> None:
         self.base = base
-        self.segments = list_seg(Segment_Number,lenght)
-    
-    def update_forward(self):
+        self.segments = list_seg(Segment_Number,length)
+
+    def update_forward(self, reach_for):
         seglist = self.segments
         # Have the first segment follow the mouse.
-        seglist[0].bpos = np.array(pg.mouse.get_pos())
-        seglist[0].apos = seglist[0].get_a()
+        seglist[0].bpos = seglist[0].get_a()
+        seglist[0].apos = np.array(reach_for)
 
         # Set every segments b position to the a position of the segment infront
         # it.
@@ -118,15 +113,24 @@ class Tentacle:
         for index, segment in enumerate(seglist):
             # Cycle through the colors with a "continuous" line between consecutive
             # colors
-            WIDTH = (1 - index/num) * 2 + index/num * 5
             if index % mod == 0:
                 i += 1
             modi = index % mod
             COLOR = ((1-modi/mod) * np.array(Colors[i-1]) + modi/mod *
                     np.array(Colors[i]))
             COLOR = tuple(COLOR)
-            segment.draw_segment(screen, COLOR, 2*i+4)
+            segment.color = COLOR
+            
 
+    def draw_tentacle(self):
+        num = len(self.segments)
+        mod = int(num / 5)
+        i = 0
+        for index,segment in enumerate(self.segments):
+            if index % mod == 0:
+                i += 1
+            segment.draw_segment(screen, segment.color, 2*i+4)
+    
 def find_bases(num,r=75):
     bases = []
     angles = np.arange(0,num) * 2*pi/num
@@ -137,27 +141,51 @@ def find_bases(num,r=75):
     
 
 if __name__ == "__main__":
+    # Initialize window size
+    size = (WIDTH, HEIGHT)
+    CENTER = np.array([WIDTH/2,HEIGHT/2])
+    # Create screen for screen
+    screen = pg.display.set_mode(size)
+    base = np.array((WIDTH/2, HEIGHT-1))
+    done = False
+    clock = pg.time.Clock()
+
+    ball = Ball(WIDTH/2,HEIGHT/2,np.array([-70.0,-50.0]),25,color=RED)
     tent_list = []
     bases = find_bases(Tentacle_Number,Radius)
     for base in bases:
         tent_list.append(Tentacle(base,Length))
+    for tentacle in tent_list:
+        tentacle.color_tentacle_rainbow()
+
+
 
  # While loop for quiting screen
     while not done:
         for event in pg.event.get():
+
             if event.type == pg.QUIT:
                 done = True
         screen.fill(GREY_LIGHT)
 
+        if Follow_Ball:
+            follow = (ball.xpos,ball.ypos)
+        else:
+            follow = pg.mouse.get_pos()
 
+        mouse = pg.mouse.get_pos()
         for tentacle in tent_list:
-            tentacle.color_tentacle_rainbow()
-            tentacle.update_forward()
+            tentacle.draw_tentacle()
+            tentacle.update_forward(follow)
             tentacle.update_backward()
 
-
-      # Update the screen
+        ball.draw_ball(screen)
+        ball.update()
+    #     # Update the screen
         pg.display.flip()
+
+
+
         clock.tick(60)
 
     pg.quit()
